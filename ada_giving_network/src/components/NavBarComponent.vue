23179a4176307d1e922d39238a9fd0c2631b52fa
Alfred Ayertey <Alfred@maximnyansa.org>
2023-05-11 22:21:31 +0000

    Setup Nav Component and Landing Page

---
+++ ada_giving_network/src/components/NavBarComponent.vue
@@ -0,0 +1,95 @@
+<template>
+    <nav>
+        <div class="logo">
+            <h2>ADA</h2>
+        </div>
+
+        <ul class="nav-links">
+            <li v-for="(item, index) in navLinks" :key="index">
+                <router-link :to="item.path"> {{ item.name }} </router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+           navLinks: [
+                {name: "SignUp", path: "/auth"},
+                {name: "SignIn", path: "/auth"},
+           ] 
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+
+<style scoped>
+    nav {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        width: 100%;
+        margin: 0 auto;
+        height: 8vh;
+        
+    }  
+
+    .logo {
+        font-family: 'Dancing Script', cursive;
+        font-size: 20px;
+        font-style: italic;
+    }
+
+    .nav-links {
+        display: flex;
+        justify-content: space-around;
+        width: 20%;
+    
+    }
+    .nav-links li {
+        list-style: none;
+    }
+
+    .nav-links a {
+        position: relative;
+        text-decoration: none;
+        letter-spacing: 2px;
+        font-weight: bold;
+        font-size: 16px;
+    }
+
+    .nav-links a::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -4px;
+    right: 0;
+    margin: 0 auto;
+    height: 3px;
+    width: 10%;
+    border-radius: 15px;
+    background-color: rgb(65, 132, 240);
+    transition: 0.25s ease-out;
+}
+
+.nav-links a:hover {
+    color: rgb(65, 132, 240);
+}
+
+.nav-links a:hover::before {
+    width: 100%;
+}
+
+
+</style>
---
+++ ada_giving_network/src/views/LandingPage.vue
@@ -0,0 +1,101 @@
+<template>
+  <main>
+      <div class="hero-section">
+
+        <header>
+        <h1> ADA Giving Network</h1>
+
+        <h3>
+          Your donation can make a world of difference. Join us in 
+          our mission to create positive change and help those in need.
+          Every contribution, no matter how small, brings us one 
+          step closer to achieving our goals and improving lives.
+        </h3>
+
+        </header>
+
+        <router-link to="/signup"><button class="btn">Donate</button></router-link>
+
+        
+      </div>
+
+        
+  </main>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style scoped>
+   main {
+    height: 92vh;
+    background: linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url("/image/hero_home.jpg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+ }
+
+.hero-section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 70%;
+}
+
+ header h1 {
+    text-align: center;
+    font-size: 50px;
+    color: rgb(255, 255, 255);
+    padding-top: 50px;
+ }
+
+ header h3 {
+  text-align: center;
+  color: white;
+  width: 50%;
+  padding-top: 10px;
+  margin: 0 auto;
+ }
+
+ .btn {
+  width: 250px;
+  margin-top: 45px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  /* background-image: linear-gradient(to right ,rgb(255, 0, 0),black); */
+  font-size: 20px;
+  font-weight: bold;
+  background-color: transparent;
+  color: white;
+  box-shadow: 2px 2px 4px white;
+  border: 2px solid white;
+  cursor: pointer;
+ }
+
+ .btn:hover {
+  background-color: white;
+  color: black;
+  box-shadow: 6px 6px 8px;
+ }
+
+ 
+
+
+</style>
